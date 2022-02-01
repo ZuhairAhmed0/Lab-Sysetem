fc72c94d3ccaaa57f1cf50746cb05e22a87f3dd1
ZuhairAhmed0 <zuh12390@gmail.com>
2022-02-01 13:10:42 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 const usersData = {username: 'zuhair', password: '12345678'};
 
-// post("/api/login") {username: string; password: string}.
+// post("/api/login") {username: string; password: string}
 
-fetch('https://lab01.deno.dev/api/login')
-	.then((response) => response.json())
-	.then((data) => {
+fetch('https://lab01.deno.dev/api/login', {
+   method: 'POST',
+   body: JSON.stringify(usersData)
+}).then((response) => response.json()).then((data) => {
 		console.log('Success:', data);
-	});
+	})
+
+// fetch('https://lab01.deno.dev/api/users', {
+// 	method: 'get',
+// 	headers: {
+// 		'Content-Type': 'application/json',
+// 	}
+// })
+// 	.then((response) => response.json())
+// 	.then((data) => {
+// 		console.log(data);
+// 	});
